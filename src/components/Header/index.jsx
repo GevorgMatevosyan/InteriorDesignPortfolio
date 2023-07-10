5aaf15f0341b3a52abdfe766eecf4d70da457321
GevorgMatevosyan <gevmatevosyan86@gmail.com>
2023-07-10 09:29:06 +0400

    start creat design portfolio

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <div>TateDesign</div>
+            <div>
+                <div>Home</div>
+                <div>About</div>
+                <div>Portfolio</div>
+                <div>Contact</div>
+                <div>Services and prices</div>
+            </div>
+            <div>
+                <div>Facebook</div>
+                <div>Instagram</div>
+                <div>Messenger</div>
+                <div>Viber</div>
+                <div>WhatsUp</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
